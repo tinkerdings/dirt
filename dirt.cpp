c59d877251707e9cfd60d66fa2f2958d1b067e2f
compieler <mawid@protonmail.com>
2023-06-30 23:57:42 +0200

    move source directory/files(s) to destination

--- dirt.cpp
+++ dirt.cpp
@@ -5,7 +5,20 @@
 
 int main(int argc, char **argv)
 {
+  if(argc != 3)
+  {
+    printf("ERROR: Incorrect number of arguments\n\n");
+    printf("Description: \n\tMoves a directory and its contents\n\n");
+    printf("Usage:\n\t%s <source_dir> <target_dir>\n\n", argv[0]);
+    return 1;
+  }
 
+  if(!MoveFileEx(argv[1], argv[2], MOVEFILE_WRITE_THROUGH))
+  {
+    printf("MoveFileEx failed with error %d\n", (int)GetLastError());
+    return 1;
+  }
+  printf("%s has been moved to %s\n", argv[1], argv[2]);
 
   return 0;
 }
