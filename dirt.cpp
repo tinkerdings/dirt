fb96b216f15af1193d751dfd630e96a7b0ceed04
compieler <mawid@protonmail.com>
2023-06-30 23:38:34 +0200

    empty main

--- dirt.cpp
+++ dirt.cpp
@@ -5,37 +5,7 @@
 
 int main(int argc, char **argv)
 {
-  DWORD ret;
-  char dirName[BUFSIZE];
 
-  /* if(argc != 2) */
-  /* { */
-  /*   printf("Usage: %s <dir>\n", argv[0]); */
-  /*   return 1; */
-  /* } */
-
-  printf("Getting current directory....\n");
-
-  ret = GetCurrentDirectory(BUFSIZE, dirName);
-
-  if(!ret)
-  {
-    printf("GetCurrentDirectory failed (%d)\n", GetLastError());
-    return 1;
-  }
-
-  if(ret > BUFSIZE)
-  {
-    printf("Buffer too small. Need %d characters for current dir path name\n", ret);
-    return 1;
-  }
-
-  if(!dirName)
-  {
-    printf("Failed to get current directory name (%d)\n", GetLastError());
-    return 1;
-  }
-  printf("Current Directory: %s\n", dirName);
 
   return 0;
 }
