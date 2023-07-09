083fdd952acde60d40a58ee98f9a03403fbb71c0
compieler <mawid@protonmail.com>
2023-07-10 01:05:58 +0200

    distinguish between executable subsystems

--- dirt.cpp
+++ dirt.cpp
@@ -1,7 +1,9 @@
 #include <windows.h>
+#include <DbgHelp.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <winuser.h>
 
 #define VK_Q 0x51
 
@@ -208,13 +210,73 @@ void handleInput(Screen &screen, HANDLE stdinHandle)
               char fullPath[MAX_PATH] = {0};
               GetFullPathNameA(activeEntry.cFileName, MAX_PATH, fullPath, 0);
               printf("fullPath: %s\n", fullPath);
-              ShellExecuteA(0, 0, fullPath, 0, 0, SW_SHOW);
+              DWORD binaryType = -1;
+              // TODO: Error handling for file opening
+              if(GetBinaryTypeA(fullPath, &binaryType))
+              {
+                if(binaryType == SCS_32BIT_BINARY || binaryType == SCS_64BIT_BINARY)
+                {
+                  HANDLE exeHandle = CreateFileA(
+                    fullPath, GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0);
+                  if(exeHandle == INVALID_HANDLE_VALUE)
+                  {
+                    printf("CreateFileA failed (%lu)\n", GetLastError());
+                    break;
+                  }
+                  HANDLE exeMapping = CreateFileMappingA(
+                    exeHandle,
+                    0,
+                    PAGE_READONLY, 0, 0, 0);
+                  if(!exeMapping || exeMapping == INVALID_HANDLE_VALUE)
+                  {
+                    printf("CreateFileMappingA failed (%lu)\n", GetLastError());
+                    break;
+                  }
+                  void* exe = MapViewOfFile(
+                    exeMapping, FILE_MAP_READ, 0, 0, 0);
+                  PIMAGE_NT_HEADERS ntHeaders = ImageNtHeader(exe);
+                  switch(ntHeaders->OptionalHeader.Subsystem)
+                  {
+                    case(IMAGE_SUBSYSTEM_WINDOWS_CUI):
+                    {
+                      ShellExecuteA(0, 0, fullPath, 0, 0, SW_SHOW);
+                    } break;
+                    case(IMAGE_SUBSYSTEM_WINDOWS_GUI):
+                    {
+                      PROCESS_INFORMATION exeInfo;
+                      STARTUPINFOA startupInfo = {
+                        sizeof(STARTUPINFOA),
+                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        STARTF_USESHOWWINDOW, SW_SHOW,
+                        0, 0, 0, 0, 0
+                      };
+                      CreateProcessA(fullPath, 
+                        0, 0, 0, false,
+                        NORMAL_PRIORITY_CLASS,
+                        0, 0,
+                        &startupInfo, &exeInfo);
+                    } break;
+                  }
+                }
+              }
+              else 
+              {
+                if(ShellExecuteA(0, 0, fullPath, 0, 0, SW_SHOW) <= (HINSTANCE)32)
+                {
+                  printf("ShellExecuteA failed (%lu)\n", GetLastError());
+                  break;
+                }
+              }
             }
           } break;
           case(VK_H):
           {
               clearScreen(*globalState.currentScreen);
               setViewPathRelative(*screen.active, "..");
+              if(!SetCurrentDirectory(screen.active->path))
+              {
+                printf("Failed to set current directory (%lu)\n", GetLastError());
+              }
           } break;
         }
       } break;
