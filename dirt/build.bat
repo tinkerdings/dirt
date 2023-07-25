d0b52b28a694670cd2898d12883574c113ff45c7
Markus Sømhovd <markus.somhovd@kongsbergdigital.com>
2023-07-25 16:10:54 +0200

    add -O2 flag to compiler switches in build.bat

--- dirt/build.bat
+++ dirt/build.bat
@@ -5,6 +5,6 @@ pushd ..\build
 
 set sources=..\dirt\dirt.cpp ..\dirt\structures\hashmap.cpp ..\dirt\memory\memory.cpp ..\dirt\error\errorCode.cpp ..\dirt\input\input.cpp ..\dirt\screen\screen.cpp ..\dirt\entry\entry.cpp
 
-cl -FC -Zi  %sources% -I..\ -link shell32.lib ole32.lib dbghelp.lib Shlwapi.lib
+cl -FC -Zi -O2 %sources% -I..\ -link shell32.lib ole32.lib dbghelp.lib Shlwapi.lib
 
 popd
